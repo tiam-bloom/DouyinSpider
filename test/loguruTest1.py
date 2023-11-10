41f1118eaa9d1ce842bbf3612fd866209d5d5cf5
yujing <3036293856@qq.com>
2023-11-10 12:04:45 +0800

    feat: 新增logging loguru日志框架测试

---
+++ test/loguruTest1.py
@@ -0,0 +1,10 @@
+# _*_ coding : UTF-8 _*_
+# @Time : 2023/11/10 12:01
+# @Auther : Tiam
+# @File : loguruTest1
+# @Project : DouyinSpider
+
+from test.loguruTest import logger
+# from loguru import logger
+
+logger.info("loguruTest1")
